706770434888cea2988525d3b4aa17ddb8d4ae19
Tenzile04 <tenzileabdiyeva2004@gmail.com>
2023-11-24 13:12:09 +0400

    First-Commit

---
+++ Pustokk/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pustokk.Models;
+using System.Collections.Generic;
+
+namespace Pustokk.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Slider> Sliders { get; set; }
+    }
+}
---
+++ Pustokk/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Pustokk.Models;
+
+namespace Pustokk.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Slider> Sliders { get; set; }
+    }
+}
